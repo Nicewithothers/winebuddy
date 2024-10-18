8860cbaefc8cd3d3adc370fb0c6c5d02ed3e9041
Nicewithothers <brkristof5@gmail.com>
2024-10-18 13:30:21 +0200

    Initial state, docker and maven configured

---
+++ backend/src/main/java/com/nicewithothers/winebuddy/WinebuddyApplication.java
@@ -0,0 +1,13 @@
+package com.nicewithothers.winebuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WinebuddyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WinebuddyApplication.class, args);
+	}
+
+}
