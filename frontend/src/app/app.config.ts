64434c562d05a689d8e6b77edf425307a49160cb
Nicewithothers <brkristof5@gmail.com>
2024-11-02 17:23:15 +0100

    Configured routes, defined simple css and generated components/services

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,8 +1,16 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import {ApplicationConfig, importProvidersFrom, provideZoneChangeDetection} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import {provideHttpClient} from '@angular/common/http';
+import {LeafletModule} from '@asymmetrik/ngx-leaflet';
+import {MessageService} from 'primeng/api';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    provideHttpClient(),
+    importProvidersFrom(LeafletModule),
+    MessageService
+  ]
 };
--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import {RegisterComponent} from './register/register.component';
+import {LoginComponent} from './login/login.component';
+import {MainpageComponent} from './mainpage/mainpage.component';
+import {NotFoundComponent} from './not-found/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: 'register', component: RegisterComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'not-found', component: NotFoundComponent},
+  {path: '', component: MainpageComponent},
+  {path: '**', redirectTo: '/not-found'}
+];
